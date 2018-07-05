ac5a2cace787424b538548f3e031867d27925171
vinuesa <vinuesa@ccg.unam.mx>
2018-07-05 06:01:20 -0500

    added comments on changing permissions to docker.sock

--- docs/tema5_get_hom_get_phy/data/start_docker.sh
+++ docs/tema5_get_hom_get_phy/data/start_docker.sh
@@ -8,3 +8,14 @@
 
 docker run --rm -d -P --name data_folder -v $HOME/data_folder:/home/you/data_folder --name db -v $HOME/db:/get_homologues/db -it csicunam/get_homologues:latest /bin/bash
 
+# si te da un mensaje de error de permisos para /var/run/docker.sock
+# haz lo siguiente:
+# ls -l /var/run/docker.sock  # verifica los permisos: 
+# >>> si sale: srw-rw---- 1 root root 0 Jul  5 04:47 /var/run/docker.sock
+#     entonces debes permitir que otros tengan permisos de lectura y escritura
+#     corriendo este comando:
+# sudo chmod o+w /var/run/docker.sock
+# sudo chmod o+r /var/run/docker.sock
+# 
+# ls -l /var/run/docker.sock  # verifica los permisos: 
+# >>> srw-rw-rw- 1 root root 0 Jul  5 04:47 /var/run/docker.sock
